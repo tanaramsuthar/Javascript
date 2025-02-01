4e1c609c4e0e227550e16c8dc5c0349d85a02fe1
tanaramsuthar <tanaramsuthar01@gmail.com>
2025-02-01 10:44:53 +0530

    Add files via upload

---
+++ 02basic/04_iife.js
@@ -0,0 +1,22 @@
+// Immediately Invoked Function Expressions (IIFE)
+// IIFE is a function that is executed right after it is created
+// IIFE is a design pattern which is also known as Self-Executing Anonymous Function and contains two major parts:
+
+(function chai(){           //name iife
+    username = "tanaram suthar"
+    console.log(`hello, ${username}.`);
+})();
+
+// ( (name,age) => {           // unnamed iife
+//     console.log(`DB CONECTED TWO ${name}, ${age}`);
+// })("tanaram suthar", 19);
+
+// let val1 = 10
+// let val2 = 5
+// function addNum(num1 , num2){
+//     let total = num1 + num2
+//     return total
+// }
+// let result1 = addNum(num1, num2)
+// let result2 = addNum(4, 5)
+
